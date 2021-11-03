4e9ab34e619ac01719ad3f64928a332a06b9eb56
uzapolsky <uzapolsky@gmail.com>
2021-11-03 18:35:28 +0300

    add logger telegram bot

--- dvmn_bot.py
+++ dvmn_bot.py
@@ -1,9 +1,25 @@
-import requests
+import logging
 import os
-from dotenv import load_dotenv
-from requests.exceptions import ReadTimeout, ConnectionError
 import time
+
+import requests
 import telegram
+from dotenv import load_dotenv
+from requests.exceptions import ConnectionError, ReadTimeout
+
+
+logger = logging.getLogger('Logger')
+
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, bot_log, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.bot_log = bot_log
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.bot_log.send_message(chat_id=self.chat_id, text=log_entry)
 
 
 def send_message(answer, bot, chat_id):
@@ -23,9 +39,13 @@ def main():
     load_dotenv()
 
     bot = telegram.Bot(token=os.environ['BOT_TOKEN'])
+    bot_log = telegram.Bot(token=os.environ['BOT_LOG_TOKEN'])
     chat_id = os.environ['CHAT_ID']
     dvmn_token = os.environ['DVMN_TOKEN']
     url = 'https://dvmn.org/api/long_polling/'
+
+    logger.setLevel(logging.WARNING)
+    logger.addHandler(TelegramLogsHandler(bot_log, chat_id))
 
     headers = {
         'Authorization': f'Token {dvmn_token}',
@@ -35,21 +55,24 @@ def main():
 
     while True:
         try:
-            response = requests.get(url, headers=headers, params=payload, timeout=60)
-            response.raise_for_status()
-            answer = response.json()
-        except ReadTimeout:
-            continue
-        except ConnectionError:
-            time.sleep(30)
-            continue
+            try:
+                response = requests.get(url, headers=headers, params=payload, timeout=60)
+                response.raise_for_status()
+            except ReadTimeout:
+                continue
+            except ConnectionError:
+                logger.warning('Проблемы с интернетом')
+                time.sleep(30)
+                continue
 
-        answer = response.json()
-        if answer['status'] == 'timeout':
-            payload['timestamp'] = answer['timestamp_to_request']
-        if answer['status'] == 'found':
-            payload['timestamp'] = answer['last_attempt_timestamp']
-            send_message(answer, bot, chat_id)
+            answer = response.json()
+            if answer['status'] == 'timeout':
+                payload['timestamp'] = answer['timestamp_to_request']
+            if answer['status'] == 'found':
+                payload['timestamp'] = answer['last_attempt_timestamp']
+                send_message(answer, bot, chat_id)
+        except telegram.error.TelegramError:
+            logger.exception('Проблема с телеграммом')
 
 
 if __name__ == '__main__':
